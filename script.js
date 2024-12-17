4642132032b43a1dd3d9f0df582812c9a64f25bd
Dev Mistry <90182112+DevM15@users.noreply.github.com>
2024-12-17 11:03:12 +0530

    second commit

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ function hero() {
         document.querySelector(".rightbar span a ").innerHTML = "01 /";
         console.log("out")
     }
-}
+}    
+
+let a = 1;
+setInterval(() => {
+
+    const selectedElement = document.querySelector('.client>:first-child'); // replace with your element's selector
+    selectedElement.remove();
+    document.querySelector(".client").innerHTML += `<img src="images/${a}.png" alt="">`
+    a++;
+    if(a>5){a =1};     
+}, 6000)
